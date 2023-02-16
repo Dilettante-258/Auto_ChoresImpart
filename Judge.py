a0af8fea2b1553f621064f7a36c2033670720983
Dilettante258 <113875344+Dilettante258@users.noreply.github.com>
2023-02-16 12:34:13 +0800

    Update Judge.py

--- Judge.py
+++ Judge.py
@@ -21,9 +21,11 @@ shelfFile1 = shelve.open('roster_information')
 shelfFile2 = shelve.open('duty_roster')
 
 def inspect():
+    n = 0
     for date in shelfFile2['duty_roster']:
         if judgedate(date):
-            Send.sendmail(list(shelfFile1['roster_information'])[0], list(shelfFile1['roster_information'].values())[0])
+            Send.sendmail(list(shelfFile1['roster_information'])[n], list(shelfFile1['roster_information'].values())[n])
+        n = n + 1
 
 inspect()
 logging.info('Finished inspection.')
