20cf6bfdb607b60cdff8c07b353dcbd446330981
Dilettante258 <113875344+Dilettante258@users.noreply.github.com>
2023-03-27 16:00:08 +0800

    Update Arrange.py

--- Arrange.py
+++ Arrange.py
@@ -27,9 +27,9 @@ logging.info('Splited successfully')
 delta = datetime.timedelta(days=3)
 beginning_date = datetime.datetime(2023, 2, 13)
 duty_roster = {}
+j = 0
+time = beginning_date
 for i in range(0, 10*len(nlists), len(nlists)):
-    j = 0
-    time = beginning_date
     for person in nlists:
         time = beginning_date + delta*j
         j += 1
